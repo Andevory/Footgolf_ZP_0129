56cf46a7b12311b0d7f47afee9f9fc25a7a45e6d
Andevory <zaletnyik.peter@blathy.info>
2024-02-05 13:11:51 +0100

    Datastructure created and file read

--- Footgolf_ZP_0129/Program.cs
+++ Footgolf_ZP_0129/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] input = File.ReadAllLines("fob2016.txt");
+List<Verseny> data = new List<Verseny>();
+for (int i = 0; i < input.Length; i++)
+{
+    data.Add(new Verseny(input[i]));
+}
+
+struct Verseny
+{
+    public string Nev;
+    public string Kategoria;
+    public string EgyesuletNev;
+    public List<int> Pontok;
+
+    public Verseny(string line)
+    {
+        Pontok = new List<int>();
+        string[] splitted = line.Split(';');
+        this.Nev = splitted[0];
+        this.Kategoria = splitted[1];
+        this.EgyesuletNev = splitted[2];
+        for (int i = 3; i < splitted.Length; i++)
+        {
+            Pontok.Add(int.Parse(splitted[i]));
+        }
+    }
+}
